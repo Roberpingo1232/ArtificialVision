164f2863a8c6532b5624b71ba1b5f018f874d36b
RobertoEduardo1232 <A22310200@live.ceti.mx>
2025-06-15 20:58:07 -0600

    019_Correciones P10

--- P10/6G_22310200_P10.py
+++ P10/6G_22310200_P10.py
@@ -8,7 +8,7 @@ mask = np.zeros(img.shape[:2],np.uint8)
 bgdModel = np.zeros((1,65),np.float64)
 fgdModel = np.zeros((1,65),np.float64)
 
-rect = (50, 50, 300, 400)
+rect = (30, 30, 420, 610)
 cv2.grabCut(img,mask,rect,bgdModel,fgdModel,5,cv2.GC_INIT_WITH_RECT)
 mask2 = np.where((mask==2)|(mask==0),0,1).astype('uint8')
 img = img*mask2[:,:,np.newaxis]
